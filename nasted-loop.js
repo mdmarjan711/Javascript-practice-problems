292ebc37c32dc26a48dbaf9122b69c6f5fdb5cc5
mdmarjan711 <mdmarjan711@gmail.com>
2024-03-10 02:50:31 +0600

    code push

---
+++ nasted-loop.js
@@ -0,0 +1,17 @@
+var a = 5
+for(var i = 1; i <= a; i++){
+   var land = ''
+   for (var j = 1; j <= a; j++){
+        land += '* '
+   }
+   console.log(land)
+}
+
+var b = 5
+for( var p = 1; p <= b; p++){
+    var ff = ''
+    for( var s = 1; s <= p; s++){
+        ff += s + ' '
+    }
+    console.log(ff);
+}
--- while loop2.js
+++ while loop2.js
@@ -9,3 +9,21 @@ while( a >= 1){
     a--
 }
 
+var isRunning = true
+while(isRunning){
+  var rand = Math.floor( Math.random()*10 + 1)
+  if ( rand == 9){
+   // console.log('winner');
+    isRunning = false
+  }else{
+    //console.log('got it'+rand);
+  }
+}
+
+var b = 0
+do {
+  console.log('taka',b);
+  o++
+}
+while( b < 20)
+
